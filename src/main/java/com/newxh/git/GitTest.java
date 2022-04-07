3979ec71c5ad54aaacf8b6816ff3c084a2091547
newxh <481751265@qq.com>
2022-04-07 14:57:17 +0800

    first commit by hot-fix

--- src/main/java/com/newxh/git/GitTest.java
+++ src/main/java/com/newxh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
   public static void main(String[] args) {
     System.out.println("hello,Git!");
     System.out.println("这是第二次修改");
+    System.out.println("hello,git hot-fix");
   }
 }
